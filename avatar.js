8dc34e438c7418f158012535e08cdb7bea1f413d
Danny <doser@github.com>
2020-05-19 16:25:07 -0700

    + all bugs fix

--- avatar.js
+++
@@ -1,9 +0,0 @@
-const { MessageEmbed } = require("discord.js");
-
-module.exports = {
-  name: "avatar",
-  description: "Show your discord avatar as PM",
-  execute(message) {
-    message.reply(message.author.displayAvatarURL());
-  }
-};
