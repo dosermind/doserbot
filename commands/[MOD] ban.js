15c69a5e708d49e5945633cce162d90217c83a0b
Danny <doser@github.com>
2020-05-21 01:40:29 -0700

    +BAN

---
+++ commands/[MOD] ban.js
@@ -0,0 +1,22 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+module.exports = {
+  name: "ban",
+  description: "Ban user",
+  execute(message) {
+    
+
+if (message.guild.member(message.author).hasPermission('BAN_MEMBERS'))
+{
+  if (message.mentions.users.size === 0) { return message.channel.send('You need to ping a user!'); }
+let kickMemver = message.guild.member(message.mentions.users.first());
+if (!kickMemver) { return message.channel.send('User not found!'); }
+
+     kickMemver.ban().then((member) => {
+          message.channel.send(member.displayName + " has been successfully banned by a moderator");
+      })
+}
+else
+  message.channel.send('[ERROR] NO PERMISSION');
+  }
+}
--- commands/sobre.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: "sobre",
-  description: "Sobre o bot (COMANDO TEMPORARIO)",
-  execute(message) {
-    return message.reply(
-      "Sou um que que está a ser desenvolvido pelo doser | Faz .invite para mais info"
-    );
-  }
-};
